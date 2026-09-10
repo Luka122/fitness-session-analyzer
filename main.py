9b306683a4486506d557411a9ad3883a2a302a57
Luka <dordeluk1@gmail.com>
2026-09-10 21:14:42 +0200

    Add fitness, participant and observation class

---
+++ main.py
@@ -0,0 +1,22 @@
+class Participant:
+    def __init__(self, participant_id, baseline_heart_rate, baseline_skin_response, baseline_temperature):
+        self.participant_id = participant_id
+        self.baseline_heart_rate = baseline_heart_rate
+        self.baseline_skin_response = baseline_skin_response
+        self.baseline_temperature = baseline_temperature
+
+
+class Observation:
+    def __init__(self, timestamp, heart_rate, skin_response, temperature, activity_level, signal_quality):
+        self.timestamp = timestamp
+        self.heart_rate = heart_rate
+        self.skin_response = skin_response
+        self.temperature = temperature
+        self.activity_level = activity_level
+        self.signal_quality = signal_quality
+
+class FitnessSession:
+    def __init__(self, participant, observation):
+        self.participant = participant
+        self.observation = observation
+
